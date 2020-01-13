6017296af4d65f5e0874f9d755327afacaec80ea
tokyozinn <andre.yabiku@hotmail.com>
2020-01-13 15:35:30 -0300

    weigth validation

--- js/principal.js
+++ js/principal.js
@@ -10,3 +10,8 @@ var altura = tdAltura.textContent;
 var imc = peso / (altura * altura);
 var tdImc = paciente.querySelector(".info-imc");
 tdImc.textContent = imc;
+
+if (peso < 10 || peso > 250){
+    tdPeso.textContent = "Peso inválido";
+    tdImc.textContent = "Erro nos dados fornecidos";
+}  
