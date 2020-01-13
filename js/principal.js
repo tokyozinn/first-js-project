f21dadf80fc2c1504c5ca0ecb754443fcc8ee37d
tokyozinn <andre.yabiku@hotmail.com>
2020-01-13 17:45:54 -0300

    looping through the table data, calculating and validating all the pacients

--- js/principal.js
+++ js/principal.js
@@ -1,27 +1,32 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+var pacientes = document.querySelectorAll(".paciente");
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+for (var i = 0; i < pacientes.length; i++){
+    var paciente = pacientes[i];
 
-if (peso < 10 || peso > 250){
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso < 10 || peso > 250){
     tdPeso.textContent = "Peso inválido";
     pesoEhValido = false;
-}
+    }
 
-if (altura < 0 || altura > 2){
+    if (altura < 0 || altura > 2){
     tdAltura.textContent = "Altura inválida";
     alturaEhValida = false;
-}
+    }
 
-if (pesoEhValido && alturaEhValida){
+    if (pesoEhValido && alturaEhValida){
     var imc = peso / (altura * altura);
     var tdImc = paciente.querySelector(".info-imc");
-    tdImc.textContent = imc;
+    tdImc.textContent = imc.toFixed(2);
+    }
 }
