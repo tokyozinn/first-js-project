d6e6bd7cf0e4f96acdfa7d4d2151c5100727f904
tokyozinn <andre.yabiku@hotmail.com>
2020-01-16 16:36:18 -0300

    Adding a fading out transition when removing itens from the table

--- js/remove.js
+++ js/remove.js
@@ -1,7 +1,10 @@
-var pacientes = document.querySelectorAll(".paciente");
+var tabela = document.querySelector("#tabela-pacientes");
 
-pacientes.forEach(function(paciente){
-    paciente.addEventListener("dblclick", function(){
-        this.remove();
-    })
+tabela.addEventListener("dblclick", function(event){
+
+    event.target.parentNode.classList.add("fadeOut");
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    }, 500);
+    
 })
